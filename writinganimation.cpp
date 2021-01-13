2bc16f74cccf0983b3e720fd1e3bde744957c025
nelsonsaake <=>
2021-01-13 11:27:47 +0000

    commit comments

--- writinganimation.cpp
+++ writinganimation.cpp
@@ -3,26 +3,45 @@
 
 
 WritingAnimation::WritingAnimation(int duration, QString text)
-    : text(text), ticker(duration, text.size())
+    : fullText(text), ticker(duration, text.size())
 {
+    // connect()
+    // whenever ticker ticks(i.e sends a ticked signal),
+    // let this WritingAnimation write
     connect(&ticker,
             &Ticker::ticked,
             this,
-            &WritingAnimation::writeChar);
+            &WritingAnimation::write);
 }
 
-void WritingAnimation::startWriting(){
+void WritingAnimation::startWriting()
+{
+    // starts ticker and
+    // emits tell slots/listners that writing has started
+
     ticker.forward();
     emit started();
 }
 
-void WritingAnimation::writeChar(int tickCount){
+void WritingAnimation::write(int tickCount)
+{
+    // copy the first n characters from the fullText given,
+    // and emit it
+
+    // n = tickCount, tickCount start from zero so we can use
+    // it, other wise we would need to maintain our own int
+    // value
 
     QString txt;
-    for(int i=0; i<tickCount; i++){
-        txt += text[i];
+    for(int i=0; i<tickCount && i<fullText.size(); i++){
+        txt += fullText[i];
     }
 
-    if(tickCount<=text.size()) emit update(txt);
-    else emit finished();
+    // emit update if txt is fullText
+    // otherwise we are done
+    if(txt!=fullText) emit update(txt);
+    else {
+        emit finished();
+        ticker.stop();
+    }
 }
--- writinganimation.h
+++ writinganimation.h
@@ -8,30 +8,59 @@
 #include <QDebug>
 
 
-
+/**
+ * WritingAnimation
+ *
+ * WritingAnimation emits update() signal with a text, at
+ * regular interval over a given duration.
+ *
+ * Each emit carries one more character than the previous
+ * one. It starts with one character.
+ *
+ * The duration and the full text is given. The interval
+ * between each update = duration/(length of text).
+ *
+ * If the updates are connected to the setText of a label,
+ * it would look as if, the text being typed to the screen.
+ */
 class WritingAnimation : public QObject
 {
 
     Q_OBJECT
 
 public:
-    WritingAnimation(int duration = 1000, QString text="");
+    // make writing animation given duration and text
+    WritingAnimation(int duration = 1000, QString fullText="");
 
 public slots:
+    // start emiting updates
     void startWriting();
 
 private slots:
-    void writeChar(int tickCount);
+    // emit update() with text
+    // setup a write() if neccessary
+    void write(int tickCount);
 
 signals:
+    // this signal tells listeners writing has started
     void started();
 
+    // this signal gives out the next text update
     void update(QString nxt);
 
+    // this signal tells listeners writing has finished
     void finished();
 
 private:
-    QString text;
+    // the full text given at construction
+    QString fullText;
+
+    // ticker drives the writing
+    //
+    // everytime there is a tick, a signal carrying a
+    // text is emitted
+    //
+    // the number of character increases with every emit
     Ticker ticker;
 };
 
